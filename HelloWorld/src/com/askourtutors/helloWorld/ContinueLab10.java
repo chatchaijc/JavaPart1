d1cc2d9ec8f0db4e3a799b8d295e8a199206e261
chatchaijc <41831881+chatchaijc@users.noreply.github.com>
2021-04-15 22:54:07 +0700

    create continue class for lab10

---
+++ HelloWorld/src/com/askourtutors/helloWorld/ContinueLab10.java
@@ -0,0 +1,13 @@
+package com.askourtutors.helloWorld;
+//Lab10
+public class ContinueLab10 {
+	public static void main(String[] args) {
+		for (int counter = 0; counter < 10; counter++) {
+			if (counter == 2)
+
+				continue;
+			System.out.println("Counter :" + counter);
+		}
+	}
+
+}
